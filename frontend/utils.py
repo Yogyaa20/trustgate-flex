6f8fe5e943dab22a040dbf956708d03c09456a0b
Yogyaa20 <midha.yogya@gmail.com>
2026-09-20 15:58:03 +0530

    feat: point frontend to Railway backend

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "http://localhost:8000"
+BACKEND_URL = "https://trustgate-flex-production.up.railway.app"
 
 def inject_global_css():
     st.markdown("""
